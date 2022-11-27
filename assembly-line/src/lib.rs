b038ba06b7b5d0d2cfb6c6f2bcff541df64dcb57
wzrdk3lly <dkmk1149@gmail.com>
2022-11-26 21:44:01 -0500

    all test pass

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -3,9 +3,18 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    match speed{
+        1..=4 => (f64::from(speed) * f64::from(221)),
+        5..=8 => ((f64::from(speed) * f64::from(221)) * 0.90 ),
+        9..=10 => ((f64::from(speed) * f64::from(221)) * 0.77 ),
+        _ => 0.0
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    
+    
+  let prod_rate_per_hr = production_rate_per_hour(speed).trunc() as u32;
+    
+   prod_rate_per_hr / 60_u32
 }
