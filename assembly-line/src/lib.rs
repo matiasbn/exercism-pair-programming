cfe9ea59fd6db5140e5eb84357901ee5ef29fc24
Matías Barrios <matias.barriosn@gmail.com>
2022-11-29 13:33:10 -0300

    assembly line finished

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -3,9 +3,15 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    match speed {
+        0 => 0.0 as f64,
+        1..=4 => f64::from(221_u16 * speed as u16),
+        5..=8 => f64::from(221_u16 * speed as u16) * 0.9,
+        9..=10 => f64::from(221_u16 * speed as u16) * 0.77,
+        _ => 1 as f64,
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    production_rate_per_hour(speed) as u32 / 60
 }
