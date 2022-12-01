f8d1ab78aec64d46e82ba7e912ea53006ea71267
Matías Barrios <matias.barriosn@gmail.com>
2022-12-01 14:23:09 -0300

    sublist initialized

---
+++ sublist/src/lib.rs
@@ -0,0 +1,12 @@
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    println!("{:?}",_first_list[0]);
+    Comparison::Equal
+}
