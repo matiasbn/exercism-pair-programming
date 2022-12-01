45b9e358136f488d239a2dae655e2ea73644dda8
Matías Barrios <matias.barriosn@gmail.com>
2022-12-01 15:07:36 -0300

    sublist finished

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -6,7 +6,55 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    println!("{:?}",_first_list[0]);
-    Comparison::Equal
+pub fn sublist<T: PartialEq + std::fmt::Debug + std::fmt::Display>(
+    first_list: &[T],
+    second_list: &[T],
+) -> Comparison {
+    let first_list_string = to_string(first_list);
+    let second_list_string = to_string(second_list);
+    if is_equal(first_list_string.clone(), second_list_string.clone()) {
+        Comparison::Equal
+    } else if is_sublist(first_list_string.clone(), second_list_string.clone()) {
+        Comparison::Sublist
+    } else if is_superlist(first_list_string.clone(), second_list_string.clone()) {
+        Comparison::Superlist
+    } else {
+        Comparison::Unequal
+    }
+}
+
+fn to_string<T: PartialEq + std::fmt::Debug>(list: &[T]) -> String
+where
+    T: std::fmt::Display,
+{
+    let mut list_string = String::from("");
+    for elem in list {
+        list_string += elem.to_string().as_str();
+    }
+    list_string
+}
+
+fn is_equal(first_list: String, second_list: String) -> bool {
+    first_list.len() == second_list.len() && first_list.as_str().contains(second_list.as_str())
+}
+
+fn is_sublist(first_list: String, second_list: String) -> bool {
+    first_list.len() < second_list.len() && second_list.as_str().contains(first_list.as_str())
+}
+
+fn is_superlist(first_list: String, second_list: String) -> bool {
+    first_list.len() > second_list.len() && first_list.as_str().contains(second_list.as_str())
+}
+
+#[test]
+fn test_sublist() {
+    let v: &[u32] = &[1];
+    let v: &[u32] = &[1, 2];
+    sublist(v, v);
+}
+
+#[test]
+fn test_to_string() {
+    let v: &[u32] = &[1, 2, 3];
+    to_string(v);
 }
