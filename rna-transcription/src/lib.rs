ac9ec952b5e34886af117bba06d75b0611b96e0f
Matías Barrios <matias.barriosn@gmail.com>
2023-01-26 14:32:32 -0300

    finished

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -1,21 +1,52 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Dna;
+pub struct Dna {
+    config: String,
+}
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Rna;
+pub struct Rna {
+    config: String,
+}
+
+const VALID_DNA: &[char] = &['G', 'C', 'T', 'A'];
+const VALID_RNA: &[char] = &['C', 'G', 'A', 'U'];
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        match dna.chars().find(|l| !VALID_DNA.contains(l)) {
+            Some(invalid) => {
+                let invalid_index = dna.chars().position(|c| c == invalid).unwrap();
+                Err(invalid_index)
+            }
+            None => Ok(Dna {
+                config: dna.to_string(),
+            }),
+        }
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {:?} into corresponding Rna", self);
+        Rna {
+            config: String::from_iter(self.config.chars().map(|c| match c {
+                'G' => "C",
+                'C' => "G",
+                'T' => "A",
+                'A' => "U",
+                _ => todo!(),
+            })),
+        }
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        match rna.chars().find(|l| !VALID_RNA.contains(l)) {
+            Some(invalid) => {
+                let invalid_index = rna.chars().position(|l| l == invalid).unwrap();
+                Err(invalid_index)
+            }
+            None => Ok(Rna {
+                config: rna.to_string(),
+            }),
+        }
     }
 }
