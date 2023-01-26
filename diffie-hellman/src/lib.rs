229bb111a7ab3eb299140bf2acc1edf64f93f6b6
Matías Barrios <matias.barriosn@gmail.com>
2023-01-26 16:47:20 -0300

    private key for

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,5 +1,15 @@
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    for prime in (2..p).rev() {
+        for test_value in (2..prime).rev() {
+            if prime % test_value == 0 {
+                continue;
+            }
+            if test_value == 2 {
+                return prime;
+            }
+        }
+    }
+    1
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
