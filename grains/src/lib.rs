c1b4cdc3789a64b1dcf3fdc650ffdeaf2bc30250
Matías Barrios <matias.barriosn@gmail.com>
2023-01-31 15:07:47 -0300

    grains finished

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,8 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    assert!(s <= 64 && s > 0, "Square must be between 1 and 64");
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).fold(0u64, |total, num| total + square(num))
 }
